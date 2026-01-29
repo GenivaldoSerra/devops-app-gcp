1348a883c8eaf1065dd2164f20d922b3bbf68fb7
GenivaldoSerra <genivaldo.serra@live.com>
2026-01-29 12:07:37 -0300

    refactor: Fazendo ajustes no server.js

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-// const server = app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
-// // graceful shutdown
-// process.on('SIGTERM', () => {
-//   console.log("SIGTERM recebido");
-//   server.close(() => process.exit(0));
-// });
-
-
-// Exportar app para testes
 module.exports = app;
 
 // Inicialização do Servidor
